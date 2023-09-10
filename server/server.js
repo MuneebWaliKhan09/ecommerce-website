20d039f0d245342e9091076e28e54cfd77ac74bc
MuneebShah0 <muneebwali809@gmail.com>
2023-09-10 12:36:22 +0500

    commit

--- server/server.js
+++ server/server.js
@@ -1,3 +1,23 @@
 const express = require('express');
-
+const cors = require('cors');
+const connect = require("./config/db")
+require("colors")
+require("dotenv").config()
 const app = express();
+const cookieParser  = require("cookie-parser");
+
+const port = 8009
+
+connect()
+
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+
+
+
+
+
+app.listen(()=> console.log(`server is running on port ${port} `.cyan.bold.underline))
