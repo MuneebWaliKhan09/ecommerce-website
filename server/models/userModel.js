a6d0aec67bc06cf5236a675aacacd8757b1e96fe
MuneebShah0 <muneebwali809@gmail.com>
2023-09-10 13:48:33 +0500

    commit fresh

---
+++ server/models/userModel.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide name'],
+        minlength: 5,
+        maxlength: 50
+    
+    },
+
+    email: {
+        type: String,
+        required: [true, 'Please provide email'],
+        validate: {
+            validator: validator.isEmail,
+            message: 'Please provide a valid email'
+        },
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: [true, 'Please provide password'],
+        minlength: 4,
+        select: false
+    },
+
+    role: {
+        type: String,
+        default: "user"
+    },
+
+    avatar: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            }
+
+
+        }
+    ],
+
+    CreatedAt:{
+        type: Date,
+        default: Date.now
+    },
+
+    
+
+
+})
--- server/server.js
+++ server/server.js
@@ -4,20 +4,37 @@ const connect = require("./config/db")
 require("colors")
 require("dotenv").config()
 const app = express();
-const cookieParser  = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 
-const port = 8009
+
+
+// handling uncaught exception
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`.red)
+    console.log(`Shutting down the server due to uncaught exception `)
+    process.exit(1)
+})
 
 connect()
 
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 
 
+app.listen(() => console.log(`server is running on port ${process.env.PORT} `.cyan.bold.underline))
 
 
-app.listen(()=> console.log(`server is running on port ${port} `.cyan.bold.underline))
+// unhandle promise rejection server rejection database rejection
+
+process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`.red)
+    console.log(`Shutting down the server due to unhandled promise rejection `)
+
+    server.close(() => {
+        process.exit(1)
+    })
+})
