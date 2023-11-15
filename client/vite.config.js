e9dff2d41c067410647dfe6733aad02934e40ddc
MuneebShah0 <muneebwali809@gmail.com>
2023-11-15 15:18:07 +0500

    w

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    proxy: {
-        target: 'https://ecommerce-muneeb.vercel.app', // Your server's address
-      },
-    },
   })
