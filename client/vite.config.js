be39a047c918d63cebcb26deaa51fe11f713b1ef
MuneebShah0 <muneebwali809@gmail.com>
2023-11-15 12:00:01 +0500

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       "/api": {
-        target: 'http://localhost:8009', // Your server's address
+        target: 'https://ecommerce-muneeb.vercel.app', // Your server's address
         changeOrigin: true,
       },
     },
