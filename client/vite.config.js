a20773afd4a3f113a7f1c45c680cfcf54e13974c
MuneebShah0 <muneebwali809@gmail.com>
2023-11-15 11:37:38 +0500

    comiit

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,10 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 4000, // Your configured port
     proxy: {
       "/api": {
-        target: 'http://192.168.10.6:8009', // Your server's address
+        target: 'http://localhost:8009', // Your server's address
         changeOrigin: true,
       },
     },
