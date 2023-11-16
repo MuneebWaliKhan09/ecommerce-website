43030acc1350d1af6ec576299aa52ea6d69f42d9
Muneeb Wali khan <126275491+MuneebWaliKhan09@users.noreply.github.com>
2023-11-16 11:59:57 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      // "/api": 'https://ecommerce-muneeb-backend.vercel.app', // Your server's address
-      "/api": 'http://localhost:8009', // Your server's address
+       "/api": 'https://ecommerce-muneeb-2hdf.vercel.app', // Your server's address
+     // "/api": 'http://localhost:8009', // Your server's address
     }
   },
   plugins: [react()],
