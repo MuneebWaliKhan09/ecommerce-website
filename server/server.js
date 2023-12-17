a6fbb6e2b8cdf204bd565a8fb07e9e7e41e638d3
MuneebWaliKhan09 <muneebwali809@gmail.com>
2023-12-17 09:53:17 +0500

    commit

--- server/server.js
+++ server/server.js
@@ -28,7 +28,10 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors({
+    origin: process.env.ORIGIN,
+    credentials: true
+}))
 
 
 // upload images
