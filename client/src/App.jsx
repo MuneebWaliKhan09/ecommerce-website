5ba681d459f14afd50045ff1b0d0fa16c2b0da66
MuneebWaliKhan09 <muneebwali809@gmail.com>
2024-04-19 17:22:45 +0500

    push

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   // const navigate = useNavigate()
 
   async function getStripeApiKey() {
-    const { data } = await axios.get("https://ecommerce-muneeb.vercel.app/api/stripeapikey");
+    const { data } = await axios.get("/api/stripeapikey");
 
     setStripeApiKey(data.stripeApiKey);
   }
